e92a8707004b9ea9deb537ccdab6326881bfc9b9
Peter Yuen <ppeetteerrsx@gmail.com>
2021-12-05 13:11:07 +0800

    V0.1.2 Fixed .shellrc updating and added -h option

--- simple_env_setup/main.py
+++ simple_env_setup/main.py
@@ -1,17 +1,17 @@
-from os import environ
-
 import click
 
 from simple_env_setup.recipes.rust import (install_rust, install_rust_all,
                                            install_rust_libs)
 from simple_env_setup.recipes.zsh import (install_starship, install_zsh,
                                           install_zsh_all)
+from simple_env_setup.utils.rc import add_rc_line, update_rc
 from simple_env_setup.utils.utils import force, no_root
 
 
-@click.group()
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
 def main():
-    pass
+    update_rc(["shared"])
+    add_rc_line("source .shellrc", "source $HOME/.local/.shellrc")
 
 
 @click.command()
